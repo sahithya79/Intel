7d20ab1b06f415f66fd8f73ab310a8f4f3d8a292
SatyaShodhaka <satyashodhaka66@gmail.com>
2019-12-18 19:24:36 +0530

    1.2

---
+++ myApp/src/pages/home.ts
@@ -0,0 +1,12 @@
+import { NavController } from '@ionic/angular';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-home',
+    templateUrl: 'home.html'
+})
+export class HomePage {
+    constructor(public navCtrl: NavController){
+
+    }
+}
